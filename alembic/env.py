be63c18c0cc7f1d2f02b46a4aef1c81b5e71b106
Tekken <maks.kreet@mail.ru>
2025-05-01 15:28:25 +0300

    Fix alembic bug in offline mode

--- alembic/env.py
+++ alembic/env.py
@@ -22,9 +22,8 @@ target_metadata = Base.metadata
 
 def run_migrations_offline():
     """Run migrations in 'offline' mode."""
-    url = config.get_main_option("sqlalchemy.url")
     context.configure(
-        url=url,
+        url=settings.DB_URL,
         target_metadata=target_metadata,
         literal_binds=True,
         dialect_opts={"paramstyle": "named"},
@@ -54,6 +53,8 @@ async def run_async_migrations():
     await connectable.dispose()
 
 if context.is_offline_mode():
+    print(2)
     run_migrations_offline()
 else:
+    print(3)
     asyncio.run(run_async_migrations())
