d299ca701ff5956439bab25334963d05593a6e01
Tekken <maks.kreet@mail.ru>
2025-05-01 14:51:54 +0300

    Added Alembic

---
+++ alembic/env.py
@@ -0,0 +1,59 @@
+from logging.config import fileConfig
+from sqlalchemy import pool
+from sqlalchemy.engine import Engine
+from sqlalchemy.ext.asyncio import create_async_engine
+import asyncio
+
+from alembic import context
+import sys
+from os.path import abspath, dirname
+
+# Добавляем путь к проекту в PYTHONPATH
+sys.path.insert(0, dirname(dirname(abspath(__file__))))
+
+from app.database import Base
+from app.models.user import User
+from app.models.task import Task
+from app.config import settings
+
+config = context.config
+fileConfig(config.config_file_name)
+target_metadata = Base.metadata
+
+def run_migrations_offline():
+    """Run migrations in 'offline' mode."""
+    url = config.get_main_option("sqlalchemy.url")
+    context.configure(
+        url=url,
+        target_metadata=target_metadata,
+        literal_binds=True,
+        dialect_opts={"paramstyle": "named"},
+    )
+
+    with context.begin_transaction():
+        context.run_migrations()
+
+def do_run_migrations(connection):
+    context.configure(
+        connection=connection, 
+        target_metadata=target_metadata,
+        compare_type=True,
+        render_as_batch=True
+    )
+
+    with context.begin_transaction():
+        context.run_migrations()
+
+async def run_async_migrations():
+    """Run migrations in 'online' mode with async engine."""
+    connectable = create_async_engine(settings.DB_URL)
+
+    async with connectable.connect() as connection:
+        await connection.run_sync(do_run_migrations)
+    
+    await connectable.dispose()
+
+if context.is_offline_mode():
+    run_migrations_offline()
+else:
+    asyncio.run(run_async_migrations())
